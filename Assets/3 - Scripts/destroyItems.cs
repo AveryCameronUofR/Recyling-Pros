56816a592dc638ff79a6fff1a0d2ff4c1d441bc1
rowbothn <32527320+rowbothn@users.noreply.github.com>
2021-02-27 17:04:18 -0600

    Conveyor end despawns items

---
+++ Assets/3 - Scripts/destroyItems.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyItems : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "grabbable")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
