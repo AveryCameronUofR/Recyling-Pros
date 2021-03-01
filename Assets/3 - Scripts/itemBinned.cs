6c5962c6672d87fe6ad889071425844633c92d52
AveryCameronUofR <35551198+AveryCameronUofR@users.noreply.github.com>
2021-03-01 17:32:49 -0600

    Updated recycling bins and made them grabbable

--- Assets/3 - Scripts/itemBinned.cs
+++ Assets/3 - Scripts/itemBinned.cs
@@ -9,7 +9,6 @@ public class itemBinned : MonoBehaviour
         if (col.gameObject.tag == "grabbable")
         {
             GameManager.gm.itemBinned();
-            Debug.Log("TEST");
             Destroy(col.gameObject);
         }
     }
