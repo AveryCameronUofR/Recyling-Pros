5b541ea03ab0af9d8829e21a6217d0068f84fbb6
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-03 14:48:07 -0600

    Added popcan explosion effect

--- Assets/3 - Scripts/popcanBehaviour.cs
+++ Assets/3 - Scripts/popcanBehaviour.cs
@@ -6,25 +6,44 @@ using Valve.VR;
 
 public class popcanBehaviour : MonoBehaviour
 {
-    public float maxVelocity = 3.0f;
+    public float maxVelocity;
+    public float safetyDelay;
+    public float shakeSpeed;
     public Material initialMat;
     public Material popcanExplodeMat;
+    public GameObject explosion;
 
     public bool inHand { get; set; }
 
     private Rigidbody rb;
-    private float fadeAmount;
-    
+    private float currDelayTime;
+    private int shakeDirection = 1;
+    private float shakeTime;
+
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        currDelayTime = safetyDelay;
+        shakeTime = shakeSpeed;
     }
 
     private void Update()
     {
         if (inHand)
         {
+            // ShakeMe();
             ChangeMaterial(rb.velocity.magnitude);
+            
+            currDelayTime -= Time.deltaTime;
+            if (currDelayTime <= 0)
+            {
+                CheckIfExploding();
+            }
+        }
+        else
+        {
+            if (currDelayTime < safetyDelay)
+                currDelayTime = safetyDelay;
         }
     }
 
@@ -47,5 +66,41 @@ public class popcanBehaviour : MonoBehaviour
         {
             r.material = new Material(initialMat);
         }
+    }
+
+    private void ShakeMe()
+    {
+        shakeTime -= Time.deltaTime;
+        if (shakeTime <= 0)
+        {
+            float shakeAmount = rb.velocity.magnitude / maxVelocity;
+
+            Quaternion currQuaternion = gameObject.transform.rotation;
+            Quaternion newQuaternion = currQuaternion;
+
+            shakeDirection *= -1;
+
+            newQuaternion.x += shakeDirection * shakeAmount;
+            newQuaternion.y += shakeDirection * shakeAmount;
+            newQuaternion.z += shakeDirection * shakeAmount;
+
+            gameObject.transform.rotation = newQuaternion;
+
+            shakeTime = shakeSpeed;
+        }
+    }
+
+    private void CheckIfExploding()
+    {
+        if (rb.velocity.magnitude > (maxVelocity * 0.75f))
+        {
+            Invoke("DestroyNow", 0.5f);
+        }
+    }
+
+    private void DestroyNow()
+    {
+        Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
