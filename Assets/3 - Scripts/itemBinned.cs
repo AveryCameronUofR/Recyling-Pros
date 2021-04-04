53a1f66c8807a64b640c5d49179489b903f2b10f
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-04 16:12:03 -0600

    Changed level size to be smaller

--- Assets/3 - Scripts/itemBinned.cs
+++ Assets/3 - Scripts/itemBinned.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Linq;
 
 public class itemBinned : MonoBehaviour
 {
+    private List<string> allList;
+
+    private void Start()
+    {
+        allList = GameManager.gm.goodItems.Concat(GameManager.gm.badItems).ToList();
+    }
+
     void OnTriggerEnter(Collider col)
     {
         if (gameObject.CompareTag("goodItem"))
@@ -14,20 +22,20 @@ public class itemBinned : MonoBehaviour
                 GameManager.gm.IncreaseScore();
                 Destroy(col.gameObject);
             }
-            else
+            else if (GameManager.gm.badItems.Contains(col.gameObject.tag))
             {
                 GameManager.gm.DecreaseScore();
                 Destroy(col.gameObject);
             }
         }
-        else
+        else if (gameObject.CompareTag("badItem"))
         {
             if (GameManager.gm.badItems.Contains(col.gameObject.tag))
             {
                 GameManager.gm.IncreaseScore();
                 Destroy(col.gameObject);
             }
-            else
+            else if (GameManager.gm.goodItems.Contains(col.gameObject.tag))
             {
                 GameManager.gm.DecreaseScore();
                 Destroy(col.gameObject);
---
+++ Assets/3 - Scripts/spawnTool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnTool : MonoBehaviour
+{
+    public GameObject toolToSpawn;
+
+    private bool itemSpawned;
+
+    private void Start()
+    {
+        itemSpawned = false;
+        CreateATool();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!itemSpawned)
+        {
+            Invoke("CreateATool", 1.0f);
+            itemSpawned = true;
+        }
+    }
+
+    private void CreateATool()
+    {
+        Vector3 spawnLoc = new Vector3(gameObject.transform.position.x - 0.3f, 
+                                        gameObject.transform.position.y, 
+                                        gameObject.transform.position.z - 0.25f);
+
+        GameObject spawnedTool = Instantiate(toolToSpawn, spawnLoc, Quaternion.identity, gameObject.transform);
+        spawnedTool.tag = toolToSpawn.gameObject.tag;
+        spawnedTool.transform.parent = gameObject.transform;
+
+        itemSpawned = false;
+    }
+}
