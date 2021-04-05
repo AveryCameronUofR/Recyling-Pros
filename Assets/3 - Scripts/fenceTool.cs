0a1e5326be7e735d2872187e4751f8618e40f8b7
Avery Cameron <35551198+AveryCameronUofR@users.noreply.github.com>
2021-04-05 13:00:19 -0600

    Added fence breaking mechanic

--- Assets/3 - Scripts/fenceTool.cs
+++ Assets/3 - Scripts/fenceTool.cs
@@ -13,7 +13,9 @@ public class fenceTool : MonoBehaviour
     private bool placed = false;
     private bool placable = false;
     private bool destroying = false;
+    private bool soundPlayed = false;
     private GameObject fenceLoc;
+    private List<GameObject> recyclingCaught = new List<GameObject>();
     void Start()
     {
         inHand = false;
@@ -25,27 +27,31 @@ public class fenceTool : MonoBehaviour
         {
             timer -= Time.deltaTime;
         }
-        if (timer <= 1 && !audioSource.isPlaying)
-        {
-            audioSource.Play();
-        }
         if (timer <= 0 && !destroying)
         {
             destroying = true;
             DestroyNow();
+        }
+        if (timer <= 1 && !audioSource.isPlaying && !soundPlayed)
+        {
+            audioSource.Play();
+            soundPlayed = true;
         }
         if (!placed && placable && !inHand)
         {
             placed = true;
             placable = false;
             this.gameObject.transform.position = fenceLoc.transform.position;
-            this.gameObject.transform.rotation = fenceLoc.transform.rotation;
             this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            Destroy(this.gameObject.GetComponent<Interactable>());
+            transform.rotation = new Quaternion(0, 90, 0, 0);
+            transform.Rotate(0, 90, 0);
+            
+            Destroy(this.gameObject.GetComponent<InteractableHoverEvents>());
             Destroy(this.gameObject.GetComponent<Throwable>());
+            Destroy(this.gameObject.GetComponent<Interactable>());
         }
     }
-
+    #region Placement Triggers
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "FencePlacement" && inHand)
@@ -64,9 +70,25 @@ public class fenceTool : MonoBehaviour
             placable = false;
         }
     }
-
+    #endregion
+    #region Recycling Triggers
+    public void OnCollisionEnter(Collision collision)
+    {
+        //Freeze all recycling that touches the net
+        if (GameManager.gm.goodItems.Contains(collision.gameObject.tag) || GameManager.gm.badItems.Contains(collision.gameObject.tag))
+        {
+            recyclingCaught.Add(collision.gameObject);
+            collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+    #endregion
     private void DestroyNow()
     {
+        Debug.Log("DESTROYING");
+        foreach (GameObject recyclingObject in recyclingCaught)
+        {
+            recyclingObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
         //Instantiate(destroying, gameObject.transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
